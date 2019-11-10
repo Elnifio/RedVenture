c1316ac57216c670b72dd3c6a3c61f4e07d92e8f
Vol0324 <flight0324@yahoo.com>
2019-11-10 16:55:56 -0500

    word cloud

--- CrawlPicture.py
+++ CrawlPicture.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
-import requests, json
+import requests
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
-def getMoviePoster(moviename = "Da Vinci Code"):
+def getMoviePoster(moviename):
     url = "http://global.bing.com/images/search?q=" + moviename + " poster&qft=+filterui:imagesize-large&FORM=R5IR5"
     soup = BeautifulSoup(requests.get(url).text, features="html5lib")
     images = [a["src"] for a in soup.find_all("img", {"alt": " "})]
@@ -25,6 +29,27 @@ def getDictionary():
         movieDictionary[i['title']]['imdb'] = i['imdb']
     return movieDictionary
 
+def getComment(movieName):
+    global stopwords
+    imdbID = "tt4633694"
+    # get the movie poster here, modify here
+    colorArray = np.array(Image.open("th.jpeg"))
+
+    posterColor = ImageColorGenerator(colorArray)
+    url = "https://www.imdb.com/title/" + imdbID + "/reviews?ref_=tt_urv"
+    soup = BeautifulSoup(requests.get(url).text, features='html5lib')
+    comments = [a for a in soup.find_all("div", class_='text show-more__control')]
+    length = max(25, len(comments))
+    comments = comments[:length]
+    commentsWithoutTag = [comment.get_text() for comment in comments]
+    commentsStr = " ".join(commentsWithoutTag)
+    wc = WordCloud(stopwords = stopwords, background_color="white", mask=colorArray).generate(commentsStr)
+    plt.imshow(wc.recolor(color_func=posterColor), interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+
 
 dictionary = getDictionary()
-print(dictionary)
+stopwords = set(STOPWORDS)
+stopwords.update(["movie", "film", "series", "show", "done", "watch", "see", "us", "story", "scene", "many"])
+getComment("El Camino: A Breaking Bad Movie")
