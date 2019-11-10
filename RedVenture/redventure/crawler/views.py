1a9e196d66138569a14c361e0f213e93d458bd24
Elnifio <elnifio0519@gmail.com>
2019-11-10 14:30:30 -0500

    Added Seaerch Page

--- RedVenture/redventure/crawler/views.py
+++ RedVenture/redventure/crawler/views.py
@@ -16,18 +16,26 @@ def search(request):
     else:
             message = ""
             has_content = False
-    image_link = ""
+    # image_link = "https://www.bing.com/th?id=OIP.9BgnL75oBYrWpn7bZ069YwHaE8&pid=Api&rs=1"
+    image_link = "https://www.bing.com/th?id=OIP.2_3PyPfpiv3Ke8zDx_PxFAHaJ4&w=137&h=183&c=7&o=5&dpr=2&pid=1.7"
     description = ""
     imdb = ""
     streaming_platform = ""
-    production_companies = ""
-    rating = ""
-    title = ""
-    release_date = ""
+    production_companies = ["company 1", "company 2", "company 3"]
+    rating = "R"
+    title = "这里填充title"
+    release_date = "Aug 28, 9102"
     original_language = ""
     popularity = ""
 
-    return render(request, 'crawler/Search.html', {'message': message})
+    return render(request, 'crawler/Search.html', {
+        'message': message,
+        "image": image_link,
+        'title': title, 
+        'date': release_date, 
+        'companies': production_companies, 
+        'rate': rating
+        })
 
 def getResultByIMDB(imdb):
     image_link = ""
